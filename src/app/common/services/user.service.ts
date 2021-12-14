a0c6f7f61851338e64dad3658f85a67cae9dbe8d
Slava Germonenko <slava.germonenko@gmail.com>
2021-12-14 20:23:48 +0300

    Improvements for users service.

--- src/app/common/services/user.service.ts
+++ src/app/common/services/user.service.ts
@@ -1,22 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 
 import { Permissions } from '@common/enums';
+import { IUser } from '@common/interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
+  public readonly currentUser$: Observable<IUser> = of({ id: 'test' });
+
+  public readonly currentUserPermissions$: Observable<Permissions[]> = of([]);
+
   public userHasPermissionAsync(permission: Permissions): Observable<boolean> {
-    // TODO: Replace with actual permission check
-    return of(true);
+    return this.currentUserPermissions$
+      .pipe(
+        map((currentUserPerms) => currentUserPerms.includes(permission)),
+      );
   }
 
   public userHasAnyOfPermissionsAsync(permissios: Permissions[]): Observable<boolean> {
-    // TODO: Replace with actual permission check
-    return of(true);
+    return this.currentUserPermissions$
+      .pipe(
+        map((currentUserPerms) => currentUserPerms.some((p) => permissios.includes(p))),
+      );
   }
 
   public userHasPermissionsAsync(permissions: Permissions[]): Observable<boolean> {
-    // TODO: Replace with actual permission check
-    return of(true);
+    return this.currentUserPermissions$
+      .pipe(
+        map((currentUserPerms) => {
+          return permissions
+            .filter((p) => currentUserPerms.includes(p))
+            .length === permissions.length;
+        }),
+      );
   }
 }
